bec61f75b7d5a7318cd8903c9e67273985048be6
Ems <Ems>
2016-11-28 18:12:05 +0100

    updated

--- app/getPrimes.js
+++ app/getPrimes.js
@@ -1,1 +1,33 @@
 
+module.exports={
+  getPrimes: function (n){
+    if(typeof n === "number"){
+      if(n>1){
+
+        var arr=[];
+
+        for(var i=2; i<n; i++){
+          if(i==3||i==2){
+
+            arr.push(i);
+          }
+          
+          else if (!(i%2===0 || i%3===0)){
+            arr.push(i);
+          }
+         }
+            
+      return arr;
+      }
+     else{
+      return "invalid number";
+     }
+   }
+     
+    else{
+      return "value must be number";
+ 
+        }
+
+}
+}
--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,39 @@
+(function(){
+ 'use strict';
 
+ var chai = require('chai');
+ var expect = chai.expect;
+
+ var myApp = require('../app/getPrimes.js');
+
+ describe("Prime Numbers Tests", function() {
+ describe("Case for some invalid values", function() {
+
+   it("should return 'invalid number' for 0", function() {
+     expect(myApp.getPrimes(0)).to.equal('invalid number');
+   });
+
+   it("should return 'invalid number' for -7", function() {
+     expect(myApp.getPrimes(-7)).to.equal('invalid number');
+   });
+
+    it("should return 'argument must be number' for stringValue", function() {
+     expect(myApp.getPrimes("hello")).to.equal('value must be number');
+   });
+
+ });
+
+ describe("Case for correct values", function() {
+
+   it("should return [ 2, 3, 5, 7, 11, 13, 17, 19 ] for 22", function() {
+     expect(myApp.getPrimes(22)).to.eql([ 2, 3, 5, 7, 11, 13, 17, 19 ]);
+   });
+it("should return [] for 2", function() {
+     expect(myApp.getPrimes(2)).to.eql([]);
+   });
+
+ });
+
+});
+
+})();
